71d8e63277cbf6f5c1af25c6cbee2f9510078554
Cameron Dixon <cam.dixon@gmail.com>
2014-02-08 16:21:22 +0000

    Add view and fix save

---
+++ rails-nku2/app/helpers/users_helper.rb
@@ -0,0 +1,13 @@
+module UsersHelper
+  def avatar_url(user)
+    if user.image.present?
+      user.image
+    else
+      # create the md5 hash
+      hash = Digest::MD5.hexdigest(user.email)
+   
+      # compile URL which can be used in <img src="RIGHT_HERE"...
+      image_src = "http://www.gravatar.com/avatar/#{hash}"
+    end
+  end
+end
